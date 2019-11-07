6d60083a617aebe6693678d830339120035f694c
BangYoon <yoonb720@naver.com>
2019-11-07 16:06:50 +0900

    Update sort_3_insertion.c

--- Sort/sort_3_insertion.c
+++ Sort/sort_3_insertion.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
 // Insertion Sort 삽입 정렬 : 앞부터 각 숫자를 적절한 위치에 삽입하기 (필요할 때만 위치 변경!)
 // O(N) ~ O(N^2)
+
+#include<stdio.h>
 int main() {
 	int arr[10] = { 2, 9, 8, 1, 10, 6, 4, 7, 3, 5 };
 	int i, j, temp;
